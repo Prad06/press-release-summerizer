af3192977f3d9372213472778315afac15493c29
Pradnyesh Choudhari <51751752+Prad06@users.noreply.github.com>
2025-05-14 23:24:02 -0400

    changed the selinium logic

--- airflow/dags/extract_release_information_l3.py
+++ airflow/dags/extract_release_information_l3.py
@@ -11,6 +11,7 @@ import trafilatura
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from dateutil import parser
 import pytz
 
@@ -38,13 +39,14 @@ def extract_html_with_selenium(**context):
     # Configure Selenium Chrome options
     options = Options()
     options.binary_location = chrome_path
-    options.add_argument("--headless=new")
+    options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--window-size=1920,1080")
+    options.add_argument("user-agent=Mozilla/5.0")
 
-    # Initialize Selenium WebDriver
-    driver = webdriver.Chrome(service=Service(chromedriver_path), options=options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
 
     html_contents = {}
 
